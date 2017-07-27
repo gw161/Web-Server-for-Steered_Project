0825345cb0ba007b78b28a6cd7a62e32d6ce9eb5
Nina <nkonstan@gmail.com>
2017-07-27 01:27:17 +0100

    Update search_ucsc.php

--- search_ucsc.php
+++ search_ucsc.php
@@ -17,17 +17,16 @@
     <!-- Custom CSS -->
     <link href="CSS/4-col-portfolio.css" rel="stylesheet">
 
-    <!-- Bootstrap JS -->
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+	<!-- Javascript for graphing -->
+		<script src="https://d3js.org/d3.v3.js"></script>
+
 
 </head>
 
@@ -51,7 +50,8 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav">
                     <li>
-                        <a href="background.html">Background</a>
+                        <a href="background.html">Analysis Summary</a>
+                    </li>
                     <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#">Data<span class="caret"></span></a>
                          <ul class="dropdown-menu">
                               <li><a href="gene_search.php">Search Gene Data</a></li>
@@ -61,9 +61,6 @@
                          </ul>
                     </li>
                     <li>
-                        <a href="image_archive.html">Image Archive</a>
-                    </li>
-                      <li>
                         <a href="team.html">Team</a>
                     </li>
                 </ul>
@@ -79,9 +76,7 @@
         <!-- Page Heading -->
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header">Project Webserver
-                    <small>A is for Awesome</small>
-                </h1>
+
 
 <?php
 $db = parse_ini_file("../config-file.ini");
@@ -102,152 +97,32 @@ else {
 ?>
 
 <?php
-	$find_genome_ids = 'SELECT DISTINCT(genome) FROM ucsc_search;';
-	$result = $conn->query($find_genome_ids);
-?>
-
-<?php
-	$find_trim_status = 'SELECT DISTINCT(trim_status) FROM ucsc_search;';
-	$result_trim = $conn->query($find_trim_status);
-?>
-
-
-
-<?php
-	$find_pipeline_id = 'SELECT DISTINCT(pipeline) FROM ucsc_search;';
-	$result_pipeline = $conn->query($find_pipeline_id);
-?>
-
-
-  
-<h1>UCSC Search</h1>
-	<form action="<?php echo htmlspecialchars("search_ucsc.php")?>" method="post">
-	<div class="input-group" style="width: 40%; float: left">
-		<input type=text name="search" placeholder="Enter gene name" class="form-control">
-
-		<div class="input-group-btn">
-			
-		</div>
-	</div>
-
-       <select style="height: 2.4em; width: 10em;" name="genome_id">
-          <option value='All'>All Genomes</option>
-          <?php while ($row = $result -> fetch_object()): ?>
-          <option value='<?php echo $row->genome; ?>'><?php echo $row->genome; ?></option>
-		  <?php endwhile; ?>
-
-        </select>
-
-       <select style="height: 2.4em; width: 15em;" name="trim_status_id">
-          <option value='All'>Trim Status</option>
-          <?php while ($row = $result_trim -> fetch_object()): ?>
-          <option value='<?php echo $row->trim_status; ?>'><?php echo $row->trim_status; ?></option>
-		  <?php endwhile; ?>
-
-        </select>
-
-       <select style="height: 2.4em; width: 10em;" name="pipeline_id">
-          <option value='All'>All Pipelines</option>
-          <?php while ($row = $result_pipeline -> fetch_object()): ?>
-          <option value='<?php echo $row->pipeline; ?>'><?php echo $row->pipeline; ?></option>
-		  <?php endwhile; ?>
-
-        </select>
-
-
-			<button class="btn btn-default" type="submit">
-				<i class="glyphicon glyphicon-search"></i>
-			</button>
-
-
-
-</form>
-
-
-<?php
-$input = "";
-$len = "";
-$result = "";
-$table = "";
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$input = htmlspecialchars($_POST["search"]);
-	$genome_input = htmlspecialchars($_POST["genome_id"]);
-	$trim_input = htmlspecialchars($_POST["trim_status_id"]);
-	$pipeline_input = htmlspecialchars($_POST["pipeline_id"]);
-}
-$len = strlen($input);
-if ($len >= 3) {
-	$sql = "SELECT * FROM ucsc_search WHERE gene_short_name LIKE \"%$input%\"";
-	if ($genome_input != 'All') { 
-		$sql = $sql . " AND genome='$genome_input'";
-	}
-	
-	if ($trim_input != 'All') {
-		$sql = $sql . " AND trim_status='$trim_input'";
-	}
-	
-	if ($pipeline_input != 'All') {
-		$sql = $sql . " AND pipeline='$pipeline_input'";
-	}
-
+$heading='';
+	$sql = "SELECT * FROM FPKM_and_counts WHERE gene_short_name LIKE \"%$heading%\"";
 	$result = $conn->query($sql);
-	if (!$result) {
-printf("Error: %s\n", $conn->error);
-	$table = "<strong>No results</strong>";
-} 
-	
-else {
-	$table = "";
-	function create_ucsc_link($row_assoc_array, $rat_id) {
-		$row_genome = $row_assoc_array["genome"];
-		$big_data_url = $row_assoc_array["bam_file_rat_$rat_id"];
-		$gene_input = $row_assoc_array["gene_short_name"];
-		$row_pipeline = $row_assoc_array["pipeline"];
-		$row_trim_status = $row_assoc_array["trim_status"];
-		
-		return "http://genome.ucsc.edu/cgi-bin/hgTracks?db=$row_genome".
-			"&position=$gene_input".
-			"&hgct_customText=track".
-			"%20type=bam".
-			"%20name=myBigBedTrack".
-			"%20description=%22$gene_input%20$row_genome".
-			"%20pipeline $row_pipeline $row_trim_status%22%20visibility=full".
-			"%20bigDataUrl=$big_data_url";
-	
-	}
-	
-	if ($input) {
-				
-		if ($result->num_rows > 0) {
-				$table = "<thead><tr> <th>Gene Name</th> <th>Genome</th> <th>Trim Status</th> <th>Pipeline</th> <th>Rat 7973</th><th>Rat 8050</th><th>Rat 8043</th><th>Rat 8033</th><th>Rat 8059</th> </tr></thead>";
-			while ($row = $result -> fetch_assoc()) {
-				$table .= "<tbody><tr><td><a href='gene_view.php?gene=".$row["gene_short_name"]."&genome=".$row["genome"]."'>".$row["gene_short_name"]."</a></td><td>".$row["genome"]."</td><td>".$row["trim_status"]."</td><td>".$row["pipeline"]."</td><td>".$row["rat_7973"]."<a target='_blank' href='". create_ucsc_link($row, "7973") ."'><button type='button' class='btn btn-default btn-xs'>View in UCSC</button></a></td><td>".$row["rat_8050"]."<a target='_blank' href='".create_ucsc_link($row, "8050")."'><button type='button' class='btn btn-default btn-xs'>View in UCSC</button></a>
-</td><td>".$row["rat_8043"]."<a target='_blank' href='".create_ucsc_link($row, "8043")."'><button type='button' class='btn btn-default btn-xs'>View in UCSC</button></a></td><td>".$row["rat_8033"]."<a target='_blank' href='".create_ucsc_link($row, "8033")."'><button type='button' class='btn btn-default btn-xs'>View in UCSC</button></a></td><td>".$row["rat_8059"]."<a target='_blank' href='".create_ucsc_link($row, "8059")."'><button type='button' class='btn btn-default btn-xs'>View in UCSC</button></a></td></tr>";  
-			}
-		$table .= "</tbody>";
-		} 
-		else { 
-			$table = "<strong>No results</strong>"; 
-		}
-	}
-}
-}
-else {
-	print "<p style='clear: both;'>Please enter at least 3 characters.</p>";
-}
-		
+
+	$row = $result -> fetch_assoc();
+
 ?>
 
+<?php
+$heading=$_GET["gene"];
+?>
 
-<?php $conn->close();?>
+	<h1 class="page-header"><b><?php echo $heading; ?></b>
+		<small>FPKM Values (Pipeline 5) or Counts (Pipeline 6) Per Rat</small>
+	</h1>
+
+<article> 
+
+<div id="myDiv"><!-- Plotly chart will be drawn inside this DIV --></div>
 
 
 
-<div class="table-responsive">
-	<table class="table"><?php echo $table;?></table>
-</div>
+</article>
 
-<span class="col-sm-2"></span>
+
+
 
 
 
@@ -256,7 +131,7 @@ else {
         </div>
         <!-- /.row -->
 
-<br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+
 
         <hr>
 
@@ -274,11 +149,240 @@ else {
     <!-- /.container -->
 
     <!-- jQuery -->
-    <script src="js/jquery.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
     <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
 </body>
+
+
+
+
+
+
+<?php
+	$sql = "SELECT * FROM FPKM_and_counts WHERE gene_short_name LIKE \"%$heading%\"";
+	$result = $conn->query($sql);
+
+	$row = $result -> fetch_assoc();
+
+?>
+
+
+
+
+
+
+        <!-- Start insert grouped bar graph-->
+
+
+
+<head>
+  <!-- Plotly.js -->
+  <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+</head>
+
+<body>
+  
+
+  <script>
+var trace1 = {
+  x: ['Rat 7973', 'Rat 8050', 'Rat 8043', 'Rat 8033', 'Rat 8059'], 
+  y: [<?php echo $row["rat_7973_low"]; ?>, <?php echo $row["rat_8050_low"]; ?>, <?php echo $row["rat_8043_low"]; ?>, <?php echo $row["rat_8033_low"]; ?>, <?php echo $row["rat_8059_low"]; ?>], 
+  name: 'FPKM Low', 
+  type: 'bar'
+};
+
+var trace2 = {
+  x: ['Rat 7973', 'Rat 8050', 'Rat 8043', 'Rat 8033', 'Rat 8059'], 
+  y: [<?php echo $row["rat_7973"]; ?>, <?php echo $row["rat_8050"]; ?>, <?php echo $row["rat_8043"]; ?>, <?php echo $row["rat_8033"]; ?>, <?php echo $row["rat_8059"]; ?>],
+  name: 'FPKM', 
+  type: 'bar'
+};
+
+var trace3 = {
+  x: ['Rat 7973', 'Rat 8050', 'Rat 8043', 'Rat 8033', 'Rat 8059'], 
+  y: [<?php echo $row["rat_7973_high"]; ?>, <?php echo $row["rat_8050_high"]; ?>, <?php echo $row["rat_8043_high"]; ?>, <?php echo $row["rat_8033_high"]; ?>, <?php echo $row["rat_8059_high"]; ?>],
+  name: 'FPKM High', 
+  type: 'bar'
+};
+
+var data = [trace1, trace2, trace3];
+var layout = {title: 'Grouped Bar Chart', barmode: 'group'};
+
+Plotly.newPlot('myDiv', data, layout);
+  </script>
+</body>
+
+
+
+        <!-- End insert grouped bar graph-->
+
+
+        <!-- Start insert heat map-->
+
+
+
+
+
+        <!-- End insert heat map-->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		<script>
+
+
+
+
+
+
+
+var mousedata = [{
+
+	"name": "rat_7973",
+	"x": 1,
+	"expression": <?php echo $row["rat_7973"]; ?>
+},
+
+{
+	"name": "rat_8050",
+	"x": 2,
+	"expression": <?php echo $row["rat_8050"]; ?>
+
+
+},
+
+{
+	"name": "rat_8043",
+	"x": 3,
+	"expression": <?php echo $row["rat_8043"]; ?>
+
+
+},
+
+{
+	"name": "rat_8033",
+	"x": 4,
+	"expression": <?php echo $row["rat_8033"]; ?>
+
+
+},
+
+{
+	"name": "rat_8059",
+	"x": 5,
+	"expression": <?php echo $row["rat_8059"]; ?>
+
+}];
+
+// call the method below
+showScatterPlot(mousedata);
+
+
+function showScatterPlot(data) {
+    // just to have some space around items. 
+    var margins = {
+        "left": 40,
+            "right": 30,
+            "top": 30,
+            "bottom": 30
+    };
+    
+    var width = 500;
+    var height = 500;
+    
+    // this will be our colour scale. An Ordinal scale.
+    var colors = d3.scale.category10();
+
+    // we add the SVG component to the scatter-load div
+    var svg = d3.select("#scatter-load").append("svg").attr("width", width).attr("height", height).append("g")
+        .attr("transform", "translate(" + margins.left + "," + margins.top + ")");
+
+    // this sets the scale that we're using for the X axis. 
+    // the domain define the min and max variables to show. In this case, it's the min and max prices of items.
+    // this is made a compact piece of code due to d3.extent which gives back the max and min of the price variable within the dataset
+    var x = d3.scale.linear()
+        .domain(d3.extent(data, function (d) {
+        return d.x;
+    }))
+    // the range maps the domain to values from 0 to the width minus the left and right margins (used to space out the visualization)
+        .range([0, width - margins.left - margins.right]);
+
+    // this does the same as for the y axis but maps from the rating variable to the height to 0. 
+    var y = d3.scale.linear()
+        .domain(d3.extent(data, function (d) {
+        return d.expression;
+    }))
+    // Note that height goes first due to the weird SVG coordinate system
+    .range([height - margins.top - margins.bottom, 0]);
+
+    // we add the axes SVG component. At this point, this is just a placeholder. The actual axis will be added in a bit
+    svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + y.range()[0] + ")");
+    svg.append("g").attr("class", "y axis");
+
+    // this is our X axis label. Nothing too special to see here.
+    svg.append("text")
+        .attr("fill", "#414241")
+        .attr("text-anchor", "end")
+        .attr("x", width / 2)
+        .attr("y", height - 35)
+        .text("Ratty");
+
+
+    // this is the actual definition of our x and y axes. The orientation refers to where the labels appear - for the x axis, below or above the line, and for the y axis, left or right of the line. Tick padding refers to how much space between the tick and the label. There are other parameters too - see https://github.com/mbostock/d3/wiki/SVG-Axes for more information
+    var xAxis = d3.svg.axis().scale(x).orient("bottom").tickPadding(2);
+    var yAxis = d3.svg.axis().scale(y).orient("left").tickPadding(2);
+
+    // this is where we select the axis we created a few lines earlier. See how we select the axis item. in our svg we appended a g element with a x/y and axis class. To pull that back up, we do this svg select, then 'call' the appropriate axis object for rendering.    
+    svg.selectAll("g.y.axis").call(yAxis);
+    svg.selectAll("g.x.axis").call(xAxis);
+
+    // now, we can get down to the data part, and drawing stuff. We are telling D3 that all nodes (g elements with class node) will have data attached to them. The 'key' we use (to let D3 know the uniqueness of items) will be the name. Not usually a great key, but fine for this example.
+    var chocolate = svg.selectAll("g.node").data(data, function (d) {
+        return d.name;
+    });
+
+    // we 'enter' the data, making the SVG group (to contain a circle and text) with a class node. This corresponds with what we told the data it should be above.
+    
+    var chocolateGroup = chocolate.enter().append("g").attr("class", "node")
+    // this is how we set the position of the items. Translate is an incredibly useful function for rotating and positioning items 
+    .attr('transform', function (d) {
+        return "translate(" + x(d.x) + "," + y(d.expression) + ")";
+    });
+
+    // we add our first graphics element! A circle! 
+    chocolateGroup.append("circle")
+        .attr("r", 5)
+        .attr("class", "dot")
+        .style("fill", function (d) {
+            // remember the ordinal scales? We use the colors scale to get a colour for our manufacturer. Now each node will be coloured
+            // by who makes the chocolate. 
+            return colors(d.manufacturer);
+    });
+
+    // now we add some text, so we can see what each item is.
+    chocolateGroup.append("text")
+        .style("text-anchor", "middle")
+        .attr("dy", -10)
+        .text(function (d) {
+            // this shouldn't be a surprising statement.
+            return d.name;
+    });
+}
+		</script>
 
 </html>
